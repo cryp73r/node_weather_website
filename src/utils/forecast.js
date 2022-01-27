23a5a7100a5f54fd8ac9dee98b124066721819d7
Priyanshu Singh <priyanshusingh0610@gmail.com>
2022-01-27 17:09:42 +0530

    Weather App

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,5 @@
 const request = require('request')
-const keys = require('./keys') || process.env
+const keys = process.env || require('./keys')
 
 const forecast = (latitude, longitude, callback)=>{
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely,hourly,daily&appid=${keys.openweatherAppId}`
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,5 +1,5 @@
 const request = require('request')
-const keys = require('./keys') || process.env
+const keys = process.env || require('./keys')
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${keys.geocodeAccessToken}`
